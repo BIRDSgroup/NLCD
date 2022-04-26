33d4feceee9eed57865f35da4ce5330a7d08aedb
aeaswar81 <aeaswar81@gmail.com>
2022-04-27 00:28:35 +0530

    cit ind data ran

--- LinearDifferentvalues/CIT_testing/cit_generic.r
+++ LinearDifferentvalues/CIT_testing/cit_generic.r
@@ -1,9 +1,9 @@
 library(cit)
 read_data<-function(path)
 {
-  dataset<- vector("list", 121)
+  dataset<- vector("list", 144)
   con = file(path, "r")
-  for (i in 1:121)
+  for (i in 1:144)
   {
     line = readLines(con, n = 1)
     line = readLines(con, n = 1)
@@ -21,21 +21,21 @@ read_data<-function(path)
   close(con)
   return (dataset)
 }
-dataset_linear<- read_data("../testing_writingvalues_sine.txt")
+dataset_linear<- read_data("../testing_writingvalues_ind.txt")
 p_cit<-c()
 p_TL<-c()
 p_TG<-c()
 p_GL<-c()
 p_Lind<-c()
 p_res<-c()
-for(i in 1:121)
+for(i in 1:144)
 {
 temp<-as.data.frame(dataset_linear[[i]],check.rows=FALSE,check.cols=FALSE,col.names=c("L","A","B"))
 L<-temp[,1]
 A<- temp[,2]
 B<- temp[,3]
-#t<-cit.cp(L,A,B)
-t<-cit.cp(L,B,A)
+t<-cit.cp(L,A,B)
+#t<-cit.cp(L,B,A)
 p_cit<-c(p_cit,t[1])
 p_TL<-c(p_TL,t[2])
 p_TG<-c(p_TG,t[3])
@@ -49,5 +49,5 @@ else
 p_res<-c(p_res," ")
 }
 df <- data.frame(p_cit,p_TL,p_TG,p_GL,p_Lind,p_res)
-write.csv(df,"results_cit_sine_BtoA.csv",row.names = FALSE)
+write.csv(df,"results_cit_ind.csv",row.names = FALSE)
 
