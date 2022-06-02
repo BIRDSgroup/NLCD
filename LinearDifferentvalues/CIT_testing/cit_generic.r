fccfa8129511dc9231798fe4b4e3c9a7bee4cabe
aeaswar81 <aeaswar81@gmail.com>
2022-06-02 11:00:51 +0530

    CIT debug by sir added

--- LinearDifferentvalues/CIT_testing/cit_generic.r
+++ LinearDifferentvalues/CIT_testing/cit_generic.r
@@ -1,9 +1,9 @@
 library(cit)
 read_data<-function(path)
 {
-  dataset<- vector("list", 1)
+  dataset<- vector("list", 1000)
   con = file(path, "r")
-  for (i in 1:1)
+  for (i in 1:121)
   {
     line = readLines(con, n = 1)
     line = readLines(con, n = 1)
@@ -21,19 +21,19 @@ read_data<-function(path)
   close(con)
   return (dataset)
 }
-dataset_linear<- read_data("../testing_writingvalues_debugSine.txt")
+dataset_linear<- read_data("../testing_writingvalues_Linear0to1.txt")
 p_cit<-c()
 p_TL<-c()
 p_TG<-c()
 p_GL<-c()
 p_Lind<-c()
 p_res<-c()
-for(i in 1:1)
+for(i in 1:121)
 {
 temp<-as.data.frame(dataset_linear[[i]],check.rows=FALSE,check.cols=FALSE,col.names=c("L","A","B"))
-L<-temp[,1]
-A<- temp[,2]
-B<- temp[,3]
+L<-temp[,1][1:100]
+A<- temp[,2][1:100]
+B<- temp[,3][1:100]
 t<-cit.cp(L,A,B)
 #t<-cit.cp(L,B,A)
 p_cit<-c(p_cit,t[1])
@@ -49,5 +49,5 @@ else
 p_res<-c(p_res," ")
 }
 df <- data.frame(p_cit,p_TL,p_TG,p_GL,p_Lind,p_res)
-write.csv(df,"debug_cit_Sinewave.csv",row.names = FALSE)
+write.csv(df,"result_Linear100datapoints.csv",row.names = FALSE)
 
