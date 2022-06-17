b1eed907bf60bcc5e6b3b9cf36c1b6a396410fee
aeaswar81 <aeaswar81@gmail.com>
2022-06-17 11:51:26 +0530

    4 synthetic and yeast CIT simulations also done

--- LinearDifferentvalues/CIT_testing/cit_generic.r
+++ LinearDifferentvalues/CIT_testing/cit_generic.r
@@ -1,9 +1,10 @@
 library(cit)
+inputs=125
 read_data<-function(path)
 {
-  dataset<- vector("list", 100)
+  dataset<- vector("list", inputs)
   con = file(path, "r")
-  for (i in 1:100)
+  for (i in 1:inputs)
   {
     line = readLines(con, n = 1)
     line = readLines(con, n = 1)
@@ -21,33 +22,51 @@ read_data<-function(path)
   close(con)
   return (dataset)
 }
-dataset_linear<- read_data("../testing_writingvalues_Indpendent.txt")
-p_cit<-c()
-p_TL<-c()
-p_TG<-c()
-p_GL<-c()
-p_Lind<-c()
-p_res<-c()
-for(i in 1:121)
+dataset_linear<- read_data("../testing_writingvalues_half_bola.txt")
+
+p_cit<-numeric(inputs)
+p_cit[1:inputs]=-1
+p_TL<- numeric(inputs)
+p_TL[1:inputs]=-1
+p_TG<- numeric(inputs)
+p_TG[1:inputs]=-1
+p_GL<- numeric(inputs)
+p_GL[1:inputs]=-1
+p_Lind<- numeric(inputs)
+p_Lind[1:inputs]=-1
+p_TG<-numeric(inputs)
+p_res<-character(inputs)
+p_res[1:inputs]="NA"
+for(i in 1:inputs)
 {
+  print(i)
+  try(
+  {
 temp<-as.data.frame(dataset_linear[[i]],check.rows=FALSE,check.cols=FALSE,col.names=c("L","A","B"))
-L<-temp[,1][1:100]
-A<- temp[,2][1:100]
-B<- temp[,3][1:100]
+L<-temp[,1]
+A<- temp[,2]
+B<- temp[,3]
+
 t<-cit.cp(L,A,B)
+
+
 #t<-cit.cp(L,B,A)
-p_cit<-c(p_cit,t[1])
-p_TL<-c(p_TL,t[2])
-p_TG<-c(p_TG,t[3])
-p_GL<-c(p_GL,t[4])
-p_Lind<-c(p_Lind,t[5])
-if(t[1]<0.05)
+p_cit[i]<-t[1]
+p_TL[i]<-t[2]
+p_TG[i]<-t[3]
+p_GL[i]<-t[4]
+p_Lind[i]<-t[5]
+
+if( t[1]<0.05)
 {
-p_res<-c(p_res,"causal")
+p_res[i]<-"causal"
 }
 else
-p_res<-c(p_res," ")
-}
+p_res[i]<-" "
+
+  }
+  )}
+
 df <- data.frame(p_cit,p_TL,p_TG,p_GL,p_Lind,p_res)
 write.csv(df,"result_Linear100datapoints.csv",row.names = FALSE)
 
