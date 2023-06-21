29a5665079a2ae641aa328edd87799cdc69128c8
nmanikgit <49224108+nmanikgit@users.noreply.github.com>
2023-06-21 12:35:43 +0530

    Generating two random seeds for np.random and random
    Due to spawn_key, entropy is same for all child_seed SeedSequence's.

--- scripts/nlcd_simulation.py
+++ scripts/nlcd_simulation.py
@@ -45,13 +45,13 @@ def main_call(i,child_seed):
     '''
     this function is called for each of the samples using Pooling, returns the sample index,p-values and overlap score
     '''
-    #rng=np.random.default_rng(child_seed)
-    #sample_seed=rng.integers(2**32 - 1)
-    #child_seed = SeedSequence(child_seed_entropy)
-    seeds = child_seed.spawn(2)
-    np.random.seed(seeds[0])
-    random.seed(seeds[1])
-    #set_random_seed(seeds[2])  #if set_random_seed needed for ANN
+    rng=np.random.default_rng(child_seed)
+    sample_seed1=rng.integers(2**32 - 1)
+    sample_seed2=rng.integers(2**32 - 1)
+    #child_seed = SeedSequence(child_seed_entropy);  seeds = child_seed.spawn(2)
+    np.random.seed(seed1)
+    random.seed(seed2)
+    #set_random_seed(seed3)  #if set_random_seed needed for ANN
     A=np.array(dataset[i][1])
     B=np.array(dataset[i][2])
     L=np.array(dataset[i][0])
